38789928c25a8d30b73ce9ef24defe6cb9e9a5a5
braeden9 <77794108+braeden9@users.noreply.github.com>
2022-04-20 17:02:27 -0500

    [1.06] User Options - Display options - Logs with nlog choice

--- Program.cs
+++ Program.cs
@@ -15,25 +15,17 @@ namespace BlogsConsole
 
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.Name);
-                }
+                string choice;
+                do {
+                    Console.WriteLine("Select an option");
+                    Console.WriteLine("1) Display All Blogs");
+                    Console.WriteLine("2) Add Blog");
+                    Console.WriteLine("3) Create Post");
+                    Console.WriteLine("4) Display Post(s)");
+                    Console.WriteLine("Enter q to quit");
+                    choice = Console.ReadLine();
+                    logger.Info($"User selected option {choice}");
+                } while (choice.ToLower() != "q");
             }
             catch (Exception ex)
             {
